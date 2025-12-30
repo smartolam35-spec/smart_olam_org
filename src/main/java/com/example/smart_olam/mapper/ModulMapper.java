9e9d9f488e1751142bcc00c430b64c681895cd8d
IlhombekUbaydullayev <ubaydullaevilhombek681@gmail.com>
2025-12-30 06:36:40 +0500

    last12

--- src/main/java/com/example/smart_olam/mapper/ModulMapper.java
+++ src/main/java/com/example/smart_olam/mapper/ModulMapper.java
@@ -1,21 +1,25 @@
 package com.example.smart_olam.mapper;
 
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.NullValuePropertyMappingStrategy;
-
+import org.mapstruct.*;
 import com.example.smart_olam.dto.modul.ModuleCreate;
 import com.example.smart_olam.dto.modul.ModuleRespone;
 import com.example.smart_olam.dto.modul.ModuleUpdate;
 import com.example.smart_olam.model.Modul;
 
-@Mapper(componentModel = "spring",nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+@Mapper(
+    componentModel = "spring",
+    nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE
+)
 public interface ModulMapper extends BaseMapper<
     Modul,
     ModuleRespone,
     ModuleCreate,
     ModuleUpdate 
 >{
-    @Mapping(source = "course.id", target = "courseId")
+    
+    @Override
+    @Mapping(target = "courseId", source = "course.id")
     ModuleRespone toDto(Modul modul);
+    
+    // Hech qanday qo'shimcha metod kerak emas
 }
